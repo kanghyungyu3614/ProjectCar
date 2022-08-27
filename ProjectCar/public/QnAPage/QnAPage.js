ae196bed2a9a2421e441169ed7b6962e7ad906a4
hyungu1997 <kanghungu3614@gmail.com>
2022-08-28 00:55:11 +0900

    게시판 됩니다.

--- ProjectCar/public/QnAPage/QnAPage.js
+++ ProjectCar/public/QnAPage/QnAPage.js
@@ -1,20 +1,10 @@
- let QnASebTable = document.querySelector("#QnASebTable")
- console.log("2번줄 : QnASebTable",QnASebTable)
-/**
- * 
- */
- // 4. 여러개 board 객체를 저장할 배열 선언 
+
+
+// 4. 여러개 board 객체를 저장할 배열 선언 
 let boardlist = [ ] 
-let Boardnum = 0;
+// 인자로 들어갈 숫자 미리 선언
 let number
-//let QnASebTableSebNumberId = Number(document.querySelector("#QnASebTableSebNumberId").textContent);
-//	document.querySelector("#QnASebTableSebNumberId").textContent +=1
-//	console.log(`document.querySelector("#QnASebTableSebNumberId")`,document.querySelector("#QnASebTableSebNumberId").textContent)
-// js가 실행될때 함수 실행
-// 게시물출력()
- 
- 
- 
+
 
 // 3. 함수 선언 [ 글 등록 버튼을 클릭할때 마다 ] 
 function 내용추가(){
@@ -24,7 +14,7 @@ function 내용추가(){
 	let textTitle = document.getElementById('textTitle').value
 	let textContent = document.getElementById('textContent').value
 	let textContentPassword = document.getElementById('textContentPassword').value
-	
+	let fakeTextContent = ''
 	if(textContentPassword.length!==4){
 		alert("비밀번호 4자리가 아닙니다.")
 		document.getElementById('textName').value = ''
@@ -34,13 +24,20 @@ function 내용추가(){
 		return
 	}
 	else if(textContentPassword.length===4){
-
+	
+	//내용을 알수없게 만들기 
+	for(let i=0; i<textContent.length; i++){
+		fakeTextContent += "*"
+	}
+	
+	
 	// 2. 객체에 담기
 	let board = {
 		번호 : QnASebTableSebNumberId,
 		작성자 : textName ,
 		제목 : textTitle,
-		내용 : textContent,
+		진짜내용 : textContent,
+		가려진내용 : fakeTextContent,
 		비밀번호 : textContentPassword
 	}
 	// 다음 QnASebTableSebNumberId 의 숫자가 증가해야 하니까 
@@ -74,59 +71,121 @@ function 게시물출력(){
 	
 	for( let i = 0 ; i<boardlist.length ; i++ ){
 		// i는 0부터 배열의길이 까지 1씩 증가 			// 배열내 i번째 객체내 '제목' 키 호출 -> 값 이 나옴
-		html += '<tr class="QnAMainTableRowtwo" >'+
-					'<td class="QnAMainTableNumber"> '+boardlist[i].번호+' </td> '+
-					'<td class='+i+'>'+boardlist[i].작성자+'</td>'+ 
-					'<td class='+i+'>'+boardlist[i].제목+'</td> '+
-					'<td class='+i+'>'+boardlist[i].내용+'</td> '+
-					'<td>'+'<button id="ButtonAdd" onclick="AddButton('+i+')" >'+"상담내용"+'<br>'+"추가하기"+'</button>'+'<button id="ButtonDelete" onclick="DeleteButton('+i+')">'+"삭제하기"+'</button>'+'</td> '+					
-				'</tr>'
+		if(i===boardlist[i].번호){
+			number = i
+		}
+		html += `<tr class="QnAMainTableRowtwo" ><td class="QnAMainTableNumber">${boardlist[i].번호}</td>  
+		         <td id=QnATextName${i}>${boardlist[i].작성자}</td>
+		         <td id=QnATitle${i}>${boardlist[i].제목}</td>                      
+                 <td id=QnAContent${i}>${boardlist[i].가려진내용}</td>                           
+				 <td id=QnAContentSolution${i}>	
+				 <button class="QnASolutionButton" id="ButtonAdd${i}" onclick="AddButton(${i})">상담내용<br>추가하기</button>	 
+			     <button class="QnASolutionButton" id="ButtonDelete${i}" onclick="DeleteButton(${i})">삭제하기</button>                                                                 
+				 </td>
+				</tr>`
 				   
 	}
 	document.getElementById('QnAMainTable').innerHTML = html
 } // 게시물출력 함수 end  
 
 
+function AddButton(number){
+	//일단 버튼 2개를 없앤다.
+	
+	//number 밑어꺼에 포함시켜야 한다.
+	document.querySelector(`#ButtonAdd${number}`).style.display = "none"
+	document.querySelector(`#ButtonDelete${number}`).style.display = "none"	
+	
+    //비밀번호 4자리를 입력하고 엔터를 눌러주세요. 랑 비밀번호창이 필요하다.	
+	let input = document.createElement('input')
+	let span = document.createElement('span')
+	//span이랑 input id 설정하기 
+	let QnAFindSpan ="SpanPassword"+String(number)
+	let QnAFindPassword = 'CreateSolution'+String(number)
+	//span 내용과 속성 설정
+	span.innerHTML = "비밀번호 4자리를 입력하고 엔터를 눌러주세요. : "
+	span.setAttribute('id',QnAFindSpan)
+	//input 내용과 속성 설정 : id는 값비교때문 class 는 css설정때문에 적용한다.
+	input.setAttribute('type','password')
+	input.setAttribute('id',QnAFindPassword)
+	input.setAttribute('class','QnAtextContentPassword')
+	
+	let QnAFindPasswordId = `#QnAContentSolution${number}`
+
+	document.querySelector(QnAFindPasswordId).appendChild(span)
+	document.querySelector(QnAFindPasswordId).appendChild(input)
+	input.addEventListener('keydown', (e)=>{
+		if(e.keyCode ===13){
+
+			AddSolutionButton(number , input ,span )
+
+		}
+		})
+    // 1 . 비밀번호 입력하면?
+}
 
 
 
 
+function AddSolutionButton(number, input ,span){
 
+	   if(boardlist[number].비밀번호 ===input.value){
+
+			span.style.display = 'none';
+			input.style.display = 'none';
+	
+	    // 2 . 내용에 진짜 내용 할당해서 진짜 내용을 보여준다.
+		for(let i=0; i<boardlist.length; i++){
+			if(i===number){
+		    let find = `#QnAContent${number}`
+			document.querySelector(find).innerHTML = boardlist[i].진짜내용			
+			}else{
+				continue;
+			}
+		}
+		
+	    //---------------------------------여기서 막힌거 같음----------------------------
+		//4.상담글 내용 담는 용도
+		let ContentSolutionText = document.createElement('input')
+		ContentSolutionText.setAttribute("type","text")
+		ContentSolutionText.setAttribute("id",`QnAFindPassword${number}`)	
+        document.querySelector(`#QnAContentSolution${number}`).appendChild(ContentSolutionText)
+	    // 3. 상담글을 추가하는 기능 
+		let ContentSolutionButton = document.createElement('button')
+		ContentSolutionButton.textContent = "상담글을 추가하시겠습니까?"
+		ContentSolutionButton.className ="CreateSolution";
+		ContentSolutionButton.setAttribute("id",`ContentSolutionButton${number}`)
+        document.querySelector(`#QnAContentSolution${number}`).appendChild(ContentSolutionButton)
+		let SolutionQnA  = document.getElementById(`QnAContentSolution${number}`)
+		
+		
+        ContentSolutionButton.addEventListener("click",()=>{
+	        ContentSolutionButton.style.display = "none";
+	        ContentSolutionText.style.display = "none";
+            SolutionQnA.textContent = ContentSolutionText.value;
+            
+        
+        
+         })
+        
+        } //if 문 끝		
+        else if(boardlist[number].비밀번호 !== input.value){
+	     alert("비밀번호를 다시 입력하세요.")
+        }
+
+}
+//AddSolutionButton 끝
 function DeleteButton(number){
-
 	for(let i=0; i<boardlist.length; i++){
 		if(number === boardlist[i].번호){
 			boardlist[i].작성자 = '삭제된 게시물입니다.'
 			boardlist[i].제목 = '삭제된 게시물입니다.'
 			boardlist[i].내용 = '내용을 보실수 없습니다.'
 			게시물출력()
-			document.querySelector("#ButtonAdd").style.display = "none"
-			document.querySelector("#ButtonDelete").style.display = "none"
+			document.querySelector(`#QnAContentSolution${number}`).textContent = '';
+		}
+		else{
 		}
 	}
 		    	
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
